7c66572ec4460873dfbd9877c1add5b1da1ef806
Alfio <alfiocutuli08@gmail.com>
2026-05-03 20:14:07 +0200

    card,cardmanager and ui improvements

--- Assets/Scripts/data.cs
+++ Assets/Scripts/data.cs
@@ -5,4 +5,7 @@ using UnityEngine.Rendering;
 public class data : MonoBehaviour
 {
     public static bool isPaused;
+   public static int nemiciuccisi=0;
+   public static int cardCount=0;
+   public static int crescita1,crescita2;
 }
