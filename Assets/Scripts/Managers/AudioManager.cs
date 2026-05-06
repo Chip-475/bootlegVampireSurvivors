c69c4dd5de6b4c3977e26c4114250e7a0abce5f1
vittori02-ui <vittorioprivitera02@gmail.com>
2026-05-07 00:04:37 +0200

    audio2

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
-
+using UnityEngine.Audio;
+using UnityEngine.UI;
 public class AudioManager : MonoBehaviour
 {
     private static AudioManager inst;
     private AudioMixer mix;
 
     [Header("sorgenti")]
-    public AudioSource bckAudio  //musica sottofondo
-    public AudioSource audioPrefab   //effetti sonori
+    public AudioSource bckAudio;  //musica sottofondo
+    public AudioSource audioPrefab;  //effetti sonor
 
     [Header("slider")]
     [SerializeField] private Slider masterSlider;
@@ -29,20 +30,20 @@ public class AudioManager : MonoBehaviour
 
     void Start()
     {
-        EnsureSliderReferences(); // controlla se gli slider sono assegnati se no l cerca per nome nella scena
-        SyncSlidersWithSavedValues(); // imposta i valori salvati 
-        ApplyMixerVolume("master", 100);
-        ApplyMixerVolume("sfx", 100);
-        ApplyMixerVolume("music", 100);
+        //EnsureSliderReferences(); // controlla se gli slider sono assegnati se no l cerca per nome nella scena
+        //SyncSlidersWithSavedValues(); // imposta i valori salvati 
+        volume(100,"master");
+        volume(100,"sfx");
+        volume(100,"music");
     }
 
-    public void playSFX(AudioClip clip,Transform spawn,float volume=1f) //passiamo il trasform perche deve essere fatto in un punto della mappa(dove si trova il nemico)
+    public void playSFX(AudioClip clip, Transform spawn, float volume = 1f) //passiamo il trasform perche deve essere fatto in un punto della mappa(dove si trova il nemico)
     {
         AudioSource a = Instantiate(audioPrefab, spawn.position, Quaternion.identity);
         a.clip = clip;
         a.volume = volume;
         a.Play();
-        Destroy(a.gameObject,clip.length);
+        Destroy(a.gameObject, clip.length);
     }
 
     /*
@@ -50,4 +51,44 @@ public class AudioManager : MonoBehaviour
     {
         sor.PlayOneShot(clip);
     }*/
+
+    public void playMusic(AudioClip clip,float volume=1f)
+    {
+        bckAudio.clip = clip;
+        bckAudio.volume = volume;
+        bckAudio.loop = true;
+        bckAudio.Play();
+    }
+
+    public void setMaster(float vol)
+    {
+
+    }
+
+    private void volume(float vol,string funz)
+    {
+        if(vol<=0f)return;
+        mix.SetFloat(funz,Mathf.Log10(vol)*20);
+    }
+
+    private void rife()
+    {
+        if(masterSlider==null)
+        {
+            GameObject ob=GameObject.Find("master");
+            if(ob!=null)masterSlider=ob.GetComponent<Slider>();
+        }
+        if(sfxSlider==null)
+        {
+            GameObject ob=GameObject.Find("sfx");
+            if (ob != null) prefabSlider=ob.GetComponent<Slider>();
+        }
+        if(bckSlider==null)
+        {
+            GameObject ob=GameObject.Find("music");
+            if(ob!=null)bckSlider=ob.GetComponent<Slider>();
+        }
+
+
+    }
 }
