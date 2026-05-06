0fb144cbdc381c7b3a593d1978d5e03085bc323d
vittori02-ui <vittorioprivitera02@gmail.com>
2026-05-07 01:54:57 +0200

    elettroboot2

---
+++ Assets/Scripts/Cards/electroBoots.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class electroBoots : MonoBehaviour
+{
+    [SerializeField]private float danno;
+    private float tick = 0.5f;
+    private float timer = 0;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        enemyClass en = other.GetComponent<enemyClass>();
+        if (en == null) return;
+        timer+=Time.deltaTime;
+        if(timer>=tick)
+        {
+            en.onDamaged(danno);  //classe abstract o static o non abstract
+            timer=0;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        timer = 0f;
+    }
+}
