37a11ece55bc995aba5542cc3495fba024742dc0
vittori02-ui <vittorioprivitera02@gmail.com>
2026-05-09 01:49:03 +0200

    card part 3

--- Assets/Scripts/Cards/increasedLifelvl1.cs
+++ Assets/Scripts/Cards/increasedLifelvl1.cs
@@ -2,15 +2,43 @@ using UnityEngine;
 
 public class increasedLifelvl1 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [System.Serializable]
+    public struct vita
     {
-        
+        public float increHp;
     }
 
-    // Update is called once per frame
-    void Update()
+    [Header("livelli vita")]
+    public vita lvl1 = new vita { increHp = 20f };
+    public vita lvl2 = new vita { increHp = 50f };
+
+    public void applicaPot(player p,int livello)
     {
-        
+        float valore;
+        switch(livello)
+        {
+            case 1:
+                valore = lvl1.increHp;
+                break;
+            case 2:
+                valore = lvl2.increHp;
+                break;
+            default:
+                valore = lvl1.increHp;
+                break;
+        }
+        p.setHpMax(valore);
+        p.hp += valore;
+        Debug.Log("aggiunto");
+        Destroy(this);
     }
+
+    //sull'oggetto che aumenta la vita (botone)
+    /*public void livelloVita(player p,int lvl)
+    {
+        increasedLifelvl1 up = p.gameObject.AddComponent<increasedLifelvl1>();
+        up.applicaPot(p, lvl);
+    }*/
+    
+    
 }
--- Assets/Scripts/Cards/lamaRotantelvl1.cs
+++ Assets/Scripts/Cards/lamaRotantelvl1.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
 
-public class lamaRotantelvl1 : MonoBehaviour
+public class lamaRotante : MonoBehaviour
 {
-    
+    [SerializeField] private player _play;
+    [SerializeField] private float velocita;
+    [SerializeField] private float dist;
+    private float ang;
+    float x, y;
     void Start()
     {
-        
+        if (_play == null) _play = FindObjectOfType<Player>(); //cosi trova il player nella scena
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if(_play == null) return;
+        ang += velocita * Time.deltaTime;//calcolo posizioni cosi puo giraee
+        x=_play.transform.position.x+Mathf.Cos(ang*Mathf.Deg2Rad)*dist;
+        y=_play.transform.position.y + Mathf.Sin(ang * Mathf.Deg2Rad)*dist;
+        transform.position=new Vector3(x,y,0);
+        trasfrom.Rotate(Vector3.forward*500f*Time.deltaTime);//ruota su stessa
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.TryGetComponent<IDamageable>(out var bers))
+        {
+            bers.damage(_play.atk);
+            Debug.Log("Fatto");
+        }
     }
 }
