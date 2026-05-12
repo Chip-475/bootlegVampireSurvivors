fc8b0ca14b8391396762af651b94abfa3005cca4
Chip-475 <gioelegarozzo2008@gmail.com>
2026-05-12 18:15:16 +0200

    Revert "."
    This reverts commit d2679885a84a157a03c13eace5fbf6f4dd7a38a6.

--- Assets/Scripts/Cards/orbitingBlade/orbitingBlades.cs
+++ Assets/Scripts/Cards/orbitingBlade/orbitingBlades.cs
@@ -2,36 +2,15 @@ using UnityEngine;
 
 public class orbitingBlades : MonoBehaviour
 {
-    public GameObject playerObj;
-    public player player;
-    public float distance;
-    private float angle;
-    public float angSpeed;
-    float x, y;
-
+    
     void Start()
     {
-        playerObj = transform.parent.gameObject;
-        player = playerObj.GetComponent<player>();
+        
     }
+
+    // Update is called once per frame
     void Update()
     {
-        if (playerObj == null) return;
-
-        angle += angSpeed * Time.deltaTime;
-        x = playerObj.transform.position.x + Mathf.Cos(angle * Mathf.Deg2Rad) * distance;
-        y = playerObj.transform.position.y + Mathf.Sin(angle * Mathf.Deg2Rad) * distance;
-
-        transform.position = new Vector3(x, y, 0);
-        transform.Rotate(Vector3.forward * 500f * Time.deltaTime);
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.TryGetComponent<IDamageable>(out var bers))
-        {
-            bers.damage(player.atk);
-            Debug.Log("blade hit");
-        }
+        
     }
 }
