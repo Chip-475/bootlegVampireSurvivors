8ae02d96e7df031940a40a0eca522b0fd7ef8680
mastery0 <giulio.dibartolo23@gmail.com>
2026-05-14 14:54:29 +0200

    cardManager

--- Assets/Scripts/Managers/cardManager.cs
+++ Assets/Scripts/Managers/cardManager.cs
@@ -1,44 +1,92 @@
-using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class cardManager : MonoBehaviour
 {
-    public List<GameObject> cards = new List<GameObject>();
-    static List<int> cardIndexes = new List<int>();
+    [System.Serializable]
+    public class CardEntry
+    {
+        public GameObject prefab;
+        public cardClass effect;
+        public int level = 1;
+    }
+
+    public static cardManager instance;
+    public List<CardEntry> cards = new List<CardEntry>();
+    public Transform cardParent;
+    public int choices = 3;
 
     void Start()
     {
-        for(int i = 0; i < cards.Count; i++)
+        instance = this;
+    }
+
+    public void spawnCards()
+    {
+
+        List<CardEntry> availableCards = getAvailableCards();
+        int cardsToSpawn = Mathf.Min(choices, availableCards.Count);
+
+        for(int i = 0; i < cardsToSpawn; i++)
         {
-            cardIndexes.Add(i);
+            int index = Random.Range(0, availableCards.Count);
+            CardEntry entry = availableCards[index];
+            GameObject spawnedCard = Instantiate(entry.prefab, cardParent);
+
+            cardChoice choice = spawnedCard.GetComponent<cardChoice>();
+            if (choice != null)
+            {
+                choice.setup(this, entry);
+            }
+            else
+            {
+                spawnedCard.AddComponent<cardChoice>().setup(this, entry);
+            }
+
+            availableCards.RemoveAt(index);
         }
     }
 
-    void Update()
+    public void pickCard(CardEntry entry)
     {
+        if (!canSpawn(entry)) return;
 
+        entry.effect.lvl = entry.level;
+        clearSpawnedCards();
     }
 
-    public static void spawnCards()
+    private List<CardEntry> getAvailableCards()
     {
+        List<CardEntry> availableCards = new List<CardEntry>();
 
-        int[] toSpawn = new int[3];
-        for(int i = 0; i < 3; i++)
+        foreach (CardEntry entry in cards)
         {
-            var x = Random.Range(0, cardIndexes.Count);
-            toSpawn[i] = x;
-            cardIndexes.Remove(x);
+            if (canSpawn(entry))
+            {
+                availableCards.Add(entry);
+            }
         }
 
-        // Card spawn to code
+        return availableCards;
     }
-    public int countCards()
+
+    private bool canSpawn(CardEntry entry)
     {
-        int i = 0;
-        for ( i = 0; i < cards.Count; i++)
+        if (entry == null || entry.prefab == null || entry.effect == null) return false;
+
+        int maxLevel = 5;
+        if (entry.effect.lvl >= maxLevel) return false;
+        if (entry.level > maxLevel) return false;
+
+        return entry.level == entry.effect.lvl + 1;
+    }
+
+    private void clearSpawnedCards()
+    {
+
+        foreach (Transform child in cardParent)
         {
+            Destroy(child.gameObject);
         }
-        return i;
     }
 }
