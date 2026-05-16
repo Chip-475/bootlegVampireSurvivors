a1d6e83104b92983e51e0c0f811323732ff81573
Chip-475 <gioelegarozzo2008@gmail.com>
2026-05-16 16:53:08 +0200

    .

--- Assets/Scripts/Managers/spawnManager.cs
+++ Assets/Scripts/Managers/spawnManager.cs
@@ -15,12 +15,12 @@ public class spawnManager : MonoBehaviour
     }
     private void Update()
     {
-        if (enemyCount <= 0 && !isSpawning)
-        {
-            Invoke(nameof(newWave), 2.5f);
-            isSpawning = true;
-            waves++;
-        }
+        //if (enemyCount <= 0 && !isSpawning)
+        //{
+        //    Invoke(nameof(newWave), 2.5f);
+        //    isSpawning = true;
+        //    waves++;
+        //}
     }
     public void newWave()
     {
